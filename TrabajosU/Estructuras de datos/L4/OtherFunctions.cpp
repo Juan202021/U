db3ed615ad3bfa8fd7fbababfeb3b7a3727cc87a
Juan202021 <juan202021@github.com>
2023-11-22 13:21:29 -0500

    up

---
+++ TrabajosU/Estructuras de datos/L4/OtherFunctions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Juan on 11/17/2023.
+//
+
+#include "OtherFunctions.h"
---
+++ TrabajosU/Estructuras de datos/L4/OtherFunctions.h
@@ -0,0 +1,14 @@
+//
+// Created by Juan on 11/17/2023.
+//
+
+#ifndef L4_OTHERFUNCTIONS_H
+#define L4_OTHERFUNCTIONS_H
+
+
+class OtherFunctions {
+
+};
+
+
+#endif //L4_OTHERFUNCTIONS_H
